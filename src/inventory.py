a07991282e3325f0ef8bb07ece9ec3b60bbd0245
Anna Wester <annawester73@gmail.com>
2026-03-15 10:51:38 +0100

    Commit with TDD (Green) for item and inventorý

--- src/inventory.py
+++ src/inventory.py
@@ -1,2 +1,16 @@
+from src.item import Item
+
 class Inventory:
-    pass
+    def __init__(self):
+        self.items = {}
+
+    def set_item(self, name, rent_price, amount):
+        self.items[name] = Item(name, rent_price, amount)
+
+    def rent(self, item_name):
+        if item_name in self.items:
+            item = self.items[item_name]
+            item.amount -= 1
+
+    def get_amount_left(self, name):
+        return self.items[name].amount
